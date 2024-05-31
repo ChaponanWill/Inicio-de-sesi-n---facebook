d22d3a5275dadf70d42e6715cda7cd40d49cc997
chaponanwill <chapowilliam09@gmail.com>
2024-05-31 07:00:39 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site:'https://github.com/ChaponanWill',
-  base:''
+  site:'https://chaponanwill.github.io',
 });
