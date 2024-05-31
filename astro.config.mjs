c5a813e03f0c31fbcee2c323d32704b7896a3cf2
chaponanwill <chapowilliam09@gmail.com>
2024-05-31 07:22:05 -0500

    deploy III

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site:'https://chaponanwill.github.io',
+  base:'Inicio-de-sesion-facebook '
 });
